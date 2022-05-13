dd8494f82b25c731dbcd4e319804cc3387b56f6c
Simon Howe <footless@gmail.com>
2022-05-13 14:43:37 +0200

    Export node module as esm
    - Has better support by consuming tooling to "pass through" assets like
      svgs

--- ui/lib/images.ts
+++ ui/lib/images.ts
@@ -1,17 +1,17 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import bg from "data-url:../images/bg-circles.png";
-import failedSrc from "data-url:../images/failed.svg";
-import fancyCardBackgroundBlue from "data-url:../images/fancy-card-background-blue.svg";
-import fancyCardBackgroundOrange from "data-url:../images/fancy-card-background-orange.svg";
-import fancyCardBackground from "data-url:../images/fancy-card-background.svg";
-import logoSrc from "data-url:../images/logo.svg";
-import reconcileSrc from "data-url:../images/reconcile.svg";
-import signInWheel from "data-url:../images/SignInWheel.svg";
-import successSrc from "data-url:../images/success.svg";
-import suspendedSrc from "data-url:../images/suspended.svg";
-import weaveLogo from "data-url:../images/WeaveLogo.svg";
+import bg from "../images/bg-circles.png";
+import failedSrc from "../images/failed.svg";
+import fancyCardBackgroundBlue from "../images/fancy-card-background-blue.svg";
+import fancyCardBackgroundOrange from "../images/fancy-card-background-orange.svg";
+import fancyCardBackground from "../images/fancy-card-background.svg";
+import logoSrc from "../images/logo.svg";
+import reconcileSrc from "../images/reconcile.svg";
+import signInWheel from "../images/SignInWheel.svg";
+import successSrc from "../images/success.svg";
+import suspendedSrc from "../images/suspended.svg";
+import weaveLogo from "../images/WeaveLogo.svg";
 
 export default {
   bg,
